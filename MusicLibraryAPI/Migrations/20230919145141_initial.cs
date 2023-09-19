17d867ad4ac332444306da7b18744fc41a15b5ee
Ari Meyer <ari.f.meyer@gmail.com>
2023-09-19 10:21:06 -0500

    First Commit

---
+++ MusicLibraryAPI/Migrations/20230919145141_initial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicLibraryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "songs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    SongTitle = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SongArtist = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SongYear = table.Column<int>(type: "int", nullable: false),
+                    SongLength = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_songs", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "songs");
+        }
+    }
+}
---
+++ MusicLibraryAPI/Models/Song.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicLibraryAPI.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string SongTitle { get; set; }
+        public string SongArtist { get; set;}
+        public int SongYear { get; set; }
+        public string SongLength { get; set;}
+
+    }
+}
